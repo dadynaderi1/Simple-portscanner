93ea6629b60c828dc2730940aa983d5500923372
Dave <dadynaderi1@gmail.com>
2018-01-29 17:44:52 +0330

    Update PortScanner.py
    Updated code

--- PortScanner.py
+++ PortScanner.py
@@ -1,16 +1,24 @@
 import socket
+import sys
 target = input("Please enter the host: ")
 print("-"*60)
 print ("Scanning started , Please wait...")
 print("-"*60)
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = socket.gethostbyname((target))
+		 
 def scan(port):
 	try:
+		ip = socket.gethostbyname((target))
 		s.connect((ip, port))
+		s.settimeout(5)
 		return True
-	except:
+	except socket.error as err:
+		print("Can't connect to host")
+		sys.exit()
+	except  :
 		return False
+		
+		
 for port  in range (1,81):
 	if scan(port):
 		print ("Port %s is open !!!"%(port))
